97150c741149d1ec810c71a37f1bdec1f3060ec3
cyf-corner <2867173036@qq.com>
2019-10-15 22:57:19 +0800

    thymeleaf

---
+++ 04-web-thymeleaf/src/main/java/com/flyroc/web4thymeleaf/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.flyroc.web4thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName
+ * @Description //TODO
+ * @Author cyf
+ * @Date 2019/10/15$ 22:05$
+ * @Version v0.0.1
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("name", "cyf");
+        return "index";
+    }
+}
